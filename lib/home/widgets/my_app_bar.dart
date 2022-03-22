5348f1906984eeb07fd5af8ee4903f75ca7e4e9a
Julio Vitor Rufino <071444@unifeso.lan>
2022-03-22 19:25:30 -0300

    Segundo commit

--- lib/home/widgets/my_app_bar.dart
+++ lib/home/widgets/my_app_bar.dart
@@ -9,7 +9,7 @@ class MyAppBar extends StatelessWidget {
           height: MediaQuery.of(context).padding.top,
         ),
         Container(
-            color: Color.fromARGB(255, 20, 8, 126),
+            color: Color.fromARGB(255, 12, 102, 221),
             height: 100,
             child: Row(
               children: <Widget>[
