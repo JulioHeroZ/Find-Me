3a2a1480e5b8962550e82ba0463b4dc70910cee0
Julio Vitor Rufino <071444@unifeso.lan>
2022-03-22 20:48:57 -0300

    Alteração do AppBar

--- lib/home/widgets/my_app_bar.dart
+++ lib/home/widgets/my_app_bar.dart
@@ -2,24 +2,42 @@ import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          height: MediaQuery.of(context).padding.top,
-        ),
-        Container(
-            color: Color.fromARGB(255, 12, 102, 221),
-            height: 100,
-            child: Row(
-              children: <Widget>[
-                Image.asset('assets/images/logo.png'),
-                SizedBox(
-                  width: 10,
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Image.asset(
+              "assets/images/logo.png",
+              height: 200,
+              width: 200,
+            ),
+            centerTitle: true,
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 214, 166, 6),
+                    Color.fromARGB(255, 226, 122, 4)
+                  ],
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
                 ),
+              ),
+            ),
+            bottom: TabBar(
+              isScrollable: true,
+              indicatorColor: Colors.white,
+              indicatorWeight: 5,
+              tabs: [
+                Tab(icon: Icon(Icons.home), text: "Home"),
+                Tab(icon: Icon(Icons.face), text: "Login"),
+                Tab(icon: Icon(Icons.settings), text: "Config."),
               ],
-            )),
-      ],
-    );
-  }
+            ),
+            elevation: 20,
+            titleSpacing: 20,
+          ),
+          body: Container(),
+        ),
+      );
 }
