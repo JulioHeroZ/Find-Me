1e2f6db667331e6bd396362b225204727c5b1902
JulioHeroZ <juliohero64@gmail.com>
2022-04-04 10:30:31 -0300

    Integração com FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
+import 'dart:js';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nubankproject/services/auth_service.dart';
 import 'package:nubankproject/widgets/my_app_bar.dart';
-
+import 'package:provider/provider.dart';
 
 void main() async {
-
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider
-
+        ChangeNotifierProvider(create: (context) => AuthService()),
       ],
-
-  
-  child: MyApp(),);
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
