72370739c27d200e42cd2ca3c922402696b2b2ea
JulioHeroZ <juliohero64@gmail.com>
2022-06-21 17:59:47 -0300

    Integração com google maps

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,12 +1,94 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+late GoogleMapController googleMapController;
+
+_determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  Position position = await Geolocator.getCurrentPosition();
+
+  return position;
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
+  Set<Marker> markers = {};
+
   @override
   Widget build(BuildContext context) {
+    final screenWidht = MediaQuery.of(context).size.height;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            height: screenHeight,
+            width: screenWidht,
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                googleMapController = controller;
+              },
+            ),
+          )
+        ]),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          Position position = await _determinePosition();
+          googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(position.latitude, position.longitude),
+                  zoom: 14)));
+          markers.clear();
+          markers.add(Marker(
+              markerId: const MarkerId('currentLocation'),
+              position: LatLng(position.latitude, position.longitude)));
+          setState(() {});
+        },
         backgroundColor: Colors.amberAccent,
         child: const Icon(Icons.search),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:lottie/lottie.dart';
 import 'package:nubankproject/services/auth_service.dart';
 import 'package:nubankproject/widgets/my_app_bar.dart';
 import 'package:provider/provider.dart';
@@ -24,6 +27,34 @@ void main() async {
   );
 }
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5)).then((value) => Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => MyAppBar())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        height: 200.0,
+        width: 200.0,
+        child: LottieBuilder.asset('assets/animassets/mapanimation.json'),
+      ),
+    ));
+  }
+}
+
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
@@ -38,7 +69,7 @@ class MyApp extends StatelessWidget {
               debugShowCheckedModeBanner: false,
               theme: kDarkTheme,
               title: 'Find Me',
-              home: MyAppBar(),
+              home: SplashScreen(),
             ),
           );
         },
