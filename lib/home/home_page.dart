0d2fb989dacbf5601d534f03fa24e176dd6623a1
JulioHeroZ <juliohero64@gmail.com>
2022-06-29 16:53:11 -0300

    Localização atual

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -2,46 +2,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-late GoogleMapController googleMapController;
-
-_determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the
-    // App to enable the location services.
-    return Future.error('Location services are disabled.');
-  }
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
-    }
-  }
-
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately.
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-  }
-
-  Position position = await Geolocator.getCurrentPosition();
-
-  return position;
-}
-
+@override
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -51,9 +14,62 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Completer<GoogleMapController> _controller = Completer();
-  static final CameraPosition _kGooglePlex = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
-  Set<Marker> markers = {};
+  LocationData? _currentPosition;
+  LatLng? _latLong;
+  bool _locating = false;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  void initState() {
+    _getUserLocation();
+    super.initState();
+  }
+
+  Future<void> _goToCurrentPosition(LatLng latlng) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        bearing: 192.8334901395799,
+        target: LatLng(latlng.latitude, latlng.longitude),
+        //tilt: 59.440717697143555,
+        zoom: 18)));
+  }
+
+  Future<LocationData> _getLocationPermission() async {
+    Location location = Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return Future.error('Service not enabled');
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return Future.error('Permission Denied');
+      }
+    }
+
+    _locationData = await location.getLocation();
+    return _locationData;
+  }
+
+  _getUserLocation() async {
+    _currentPosition = await _getLocationPermission();
+    _goToCurrentPosition(
+        LatLng(_currentPosition!.latitude!, _currentPosition!.longitude!));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,27 +86,16 @@ class _HomePageState extends State<HomePage> {
           zoomControlsEnabled: true,
           initialCameraPosition: _kGooglePlex,
           onMapCreated: (GoogleMapController controller) {
-            googleMapController = controller;
+            _controller.complete(controller);
+          },
+          onCameraMove: (CameraPosition position) {
+            setState(() {
+              _locating = true;
+              _latLong = position.target;
+            });
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          Position position = await _determinePosition();
-          googleMapController.animateCamera(CameraUpdate.newCameraPosition(
-              CameraPosition(
-                  target: LatLng(position.latitude, position.longitude),
-                  zoom: 18)));
-          markers.clear();
-          markers.add(Marker(
-              markerId: const MarkerId('currentLocation'),
-              position: LatLng(position.latitude, position.longitude)));
-          setState(() {});
-        },
-        backgroundColor: Colors.amberAccent,
-        child: const Icon(Icons.search),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
